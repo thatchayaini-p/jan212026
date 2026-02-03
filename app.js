0020491b36c7d66a1eaab5d80a31ebacac4c1e10
Ubuntu <ubuntu@ip-172-31-42-33.eu-north-1.compute.internal>
2026-02-03 10:31:36 +0000

    newone

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('new Nodejs Project\n');
+  res.end(' Nodejs Project\n');
 });
 
 const PORT = 3000;
